a64288ae9c93b610004fe6634c242cdde694399c
KhasanTursunov <khasantursunovrustamovich@gmail.com>
2025-02-03 12:42:21 +0500

    ended

--- src/components/bestseller/Bestseller.jsx
+++ src/components/bestseller/Bestseller.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../../scss/Bestseller.scss'
 import Best1 from '../../assets/svg/best1.svg'
 import Best4 from '../../assets/svg/best4.svg'
@@ -6,7 +6,25 @@ import Blue from '../../assets/svg/blue.svg'
 import Green from '../../assets/svg/green.svg'
 import Orange from '../../assets/svg/orange.svg'
 import Black from '../../assets/svg/black.svg'
+import { request } from '../../api'
 const Bestseller = () => {
+ 
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    request
+        .get("/products", {
+          params: {
+            limit: 8,
+          },
+        })
+        .then((res) => {
+          console.log(res.data);
+          setProducts(res.data)
+        })
+        .catch((err) => console.error("API Error:", err));
+  }, []);
+   
   return (
     <>
       <section className="bestseller_section">
@@ -18,142 +36,36 @@ const Bestseller = () => {
           </div>
 
           <div className="best_wrapper">
-            <div className="best_card">
-                <div className="div_for_main_img">
-              <img src={Best4} alt="" />
-                </div>
-              <h3>Graphic Design</h3>
-              <p>English Department</p>
-              <div className="div_for_price">
-                <p className="firstP">$16.48</p>
-                <p className="secondP">$6.48</p>
-              </div>
-              <div className="div_for_colors">
-                <img src={Blue} alt="" />
-                <img src={Green} alt="" />
-                <img src={Orange} alt="" />
-                <img src={Black} alt="" />
-              </div>
-            </div>
-            <div className="best_card">
-                <div className="div_for_main_img">
-              <img src={Best4} alt="" />
-                </div>
-              <h3>Graphic Design</h3>
-              <p>English Department</p>
-              <div className="div_for_price">
-                <p className="firstP">$16.48</p>
-                <p className="secondP">$6.48</p>
-              </div>
-              <div className="div_for_colors">
-                <img src={Blue} alt="" />
-                <img src={Green} alt="" />
-                <img src={Orange} alt="" />
-                <img src={Black} alt="" />
-              </div>
-            </div>
-            <div className="best_card">
-                <div className="div_for_main_img">
-              <img src={Best4} alt="" />
-                </div>
-              <h3>Graphic Design</h3>
-              <p>English Department</p>
-              <div className="div_for_price">
-                <p className="firstP">$16.48</p>
-                <p className="secondP">$6.48</p>
-              </div>
-              <div className="div_for_colors">
-                <img src={Blue} alt="" />
-                <img src={Green} alt="" />
-                <img src={Orange} alt="" />
-                <img src={Black} alt="" />
-              </div>
-            </div>
-            <div className="best_card">
-                <div className="div_for_main_img">
-              <img src={Best4} alt="" />
-                </div>
-              <h3>Graphic Design</h3>
-              <p>English Department</p>
-              <div className="div_for_price">
-                <p className="firstP">$16.48</p>
-                <p className="secondP">$6.48</p>
-              </div>
-              <div className="div_for_colors">
-                <img src={Blue} alt="" />
-                <img src={Green} alt="" />
-                <img src={Orange} alt="" />
-                <img src={Black} alt="" />
-              </div>
-            </div>
-            <div className="best_card">
-                <div className="div_for_main_img">
-              <img src={Best4} alt="" />
-                </div>
-              <h3>Graphic Design</h3>
-              <p>English Department</p>
-              <div className="div_for_price">
-                <p className="firstP">$16.48</p>
-                <p className="secondP">$6.48</p>
-              </div>
-              <div className="div_for_colors">
-                <img src={Blue} alt="" />
-                <img src={Green} alt="" />
-                <img src={Orange} alt="" />
-                <img src={Black} alt="" />
-              </div>
-            </div>
-            <div className="best_card">
-                <div className="div_for_main_img">
-              <img src={Best4} alt="" />
-                </div>
-              <h3>Graphic Design</h3>
-              <p>English Department</p>
-              <div className="div_for_price">
-                <p className="firstP">$16.48</p>
-                <p className="secondP">$6.48</p>
-              </div>
-              <div className="div_for_colors">
-                <img src={Blue} alt="" />
-                <img src={Green} alt="" />
-                <img src={Orange} alt="" />
-                <img src={Black} alt="" />
-              </div>
-            </div>
-            <div className="best_card">
-                <div className="div_for_main_img">
-              <img src={Best4} alt="" />
-                </div>
-              <h3>Graphic Design</h3>
-              <p>English Department</p>
-              <div className="div_for_price">
-                <p className="firstP">$16.48</p>
-                <p className="secondP">$6.48</p>
-              </div>
-              <div className="div_for_colors">
-                <img src={Blue} alt="" />
-                <img src={Green} alt="" />
-                <img src={Orange} alt="" />
-                <img src={Black} alt="" />
-              </div>
-            </div>
-                <div className="best_card">
+            {products.length > 0 ? (
+              products.map((element) => (
+                <>
+                  <div className="best_card" key={element.id}>
                     <div className="div_for_main_img">
-                <img src={Best4} alt="" />
+                      <img
+                        height={250}
+                        width={250}
+                        src={element.image}
+                        alt={element.title}
+                      />
                     </div>
-                <h3>Graphic Design</h3>
-                <p>English Department</p>
-                <div className="div_for_price">
-                    <p className="firstP">$16.48</p>
-                    <p className="secondP">$6.48</p>
-                </div>
-                <div className="div_for_colors">
-                    <img src={Blue} alt="" />
-                    <img src={Green} alt="" />
-                    <img src={Orange} alt="" />
-                    <img src={Black} alt="" />
-                </div>
-                </div>
+                    <h3>{element.title}</h3>
+                    <p>{element.category}</p>
+                    <div className="div_for_price">
+                      <p className="firstP">{element.price}$</p>
+                      <p className="secondP">{element.rating.count}$</p>
+                    </div>
+                    <div className="div_for_colors">
+                      <img src={Blue} alt="" />
+                      <img src={Green} alt="" />
+                      <img src={Orange} alt="" />
+                      <img src={Black} alt="" />
+                    </div>
+                  </div>
+                </>
+              ))
+            ) : (
+              <p>Loading...</p>
+            )}
           </div>
         </div>
       </section>
